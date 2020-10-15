20cc2cec4c37ab2428b85052e7ce520279df2a0b
Opilane TTHK <opilane@tthk.ee>
2020-10-15 11:38:35 +0300

    first commit

---
+++ IDCodeReader/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace IDCodeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your personal code:");
+            string idCode = Console.ReadLine();
+            int idcodelenght = idCode.Length;
+
+            if (Validate(idCode))
+            {
+                HelloUser(idCode);
+            }
+            else
+            {
+                Console.WriteLine("wrong format.");
+            }
+        }
+        public static bool Validate(string idCode)
+        {
+            if (idCode.Length == 11)
+            {
+                try
+                {
+                    long.Parse(idCode);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                    Console.WriteLine($"Wrong format = {e}");
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static void HelloUser(string idCode)
+        {
+            int firstnum = Int32.Parse(idCode[0].ToString());
+            if (firstnum == 1 || firstnum == 3 || firstnum == 5)
+            {
+                Console.WriteLine("Hello, Sir!!");
+            }
+            else if (firstnum == 2 || firstnum == 4 || firstnum == 6)
+            {
+                Console.WriteLine("Hello, Madam!!");
+            }
+
+        }
+        
+    }
+}
---
+++ Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayLOGIT();
+            
+        }public static void DisplayLOGIT()
+        {
+            string LOGITfull = "LOGITpe20";
+            string LOGITsub = LOGITfull.Substring(0, 5);
+            string LOGITsubpe = LOGITfull.Substring(5, 2);
+            string LOGITsub20 = LOGITfull.Substring(7, 2);
+            Console.WriteLine(LOGITsub);
+            Console.WriteLine(LOGITsubpe);
+            Console.WriteLine(LOGITsub20);
+        }
+
+    }
+}
