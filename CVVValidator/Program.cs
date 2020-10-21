a4d1cab92e42d83965a03ae824da4f842f76b8c6
Opilane TTHK <opilane@tthk.ee>
2020-10-21 16:14:21 +0300

    addCVV valitation

---
+++ CVVValidator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CVVValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your CVV:");
+            string userCVV = Console.ReadLine();
+            if (ThreeNum(userCVV))
+            {
+                Console.WriteLine("alright");
+            }
+        }public static bool ThreeNum(string userCVV)
+        {
+            if (userCVV.Length == 3)
+            {
+                try
+                {
+                    Int32.Parse(userCVV);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Wrong format:{e}");
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong format!");
+                return false;
+            }
+        }
+    }
+}
